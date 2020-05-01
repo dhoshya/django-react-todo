828ee6887759a24a47043d1e3a86fd95289800a4
Rohan Joshi <joshi.rohan09@gmail.com>
2020-04-30 23:01:55 -0400

    Added djangorestframework and cors-headers
    Also created model and added data via django admin panel.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'todo',
 ]
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
